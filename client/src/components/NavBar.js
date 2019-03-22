90aece32425e4378fb627be6ddc4f89eb1b5cfb6
muzzyay <muzzyay@gmail.com>
2019-03-22 14:10:37 -0700

    changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavBar() {
     return <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <a className="navbar-brand" href="#"><img src="images/logo.png" width="100" height="30"/></a>
+    <a className="navbar-brand" href="/"><img src="images/logo.png" width="100" height="30"/></a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
