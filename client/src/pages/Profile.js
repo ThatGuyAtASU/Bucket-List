a22b3e2f9e111e58268a0a189514b36ff00d1bfc
muzzyay <muzzyay@gmail.com>
2019-03-22 13:40:27 -0700

    postItem modal and button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserNavBar from "../components/UserNavBar";
 import UserInfo from "../components/UserInfo";
+import Footer from "../components/Footer"
 
-function Profile(){
+function Profile() {
 
     return <>
-    <UserNavBar/>
-    <UserInfo/>
+        <UserNavBar />
+        <UserInfo />
+        <Footer />
     </>
 
 }
